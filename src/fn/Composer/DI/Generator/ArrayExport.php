53f0fab881b123fd4d12c536b8a7430df544d33d
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2018-08-15 14:32:57 +0200

    add class Generator\ArrayExport

---
+++ src/fn/Composer/DI/Generator/ArrayExport.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * (c) php-fn
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fn\Composer\DI\Generator;
+
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+
+/**
+ * Convert array to a short index format
+ */
+class ArrayExport
+{
+
+    /**
+     * @var array
+     */
+    private $iterable;
+
+    /**
+     * @param array $iterable
+     */
+    public function __construct(array $iterable)
+    {
+        $this->iterable = $iterable;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $tokens = ['['];
+
+        $it = new RecursiveIteratorIterator(
+            new RecursiveArrayIterator($this->iterable),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+        $depths = [$init = ['i' => 0, 'c' => 0]];
+        foreach ($it as $key => $value) {
+            $depth = $it->getDepth();
+            $lastDepth = \count($depths) - 1;
+            $depths[$depth] = $depths[$depth] ?? $init;
+            if ($depth > $lastDepth) {
+                $tokens[] = '[';
+            } else if ($depth < $lastDepth) {
+                $depths = \array_slice($depths, 0, $depth + 1, true);
+                $tokens[] = \str_repeat(']', $lastDepth - $depth);
+                $tokens[] = ', ';
+            } else if ($depths[$depth]['c']) {
+                $tokens[] = ', ';
+            }
+
+            if (!\is_numeric($key)) {
+                $tokens[] = var_export($key, true);
+                $tokens[] = ' => ';
+            } else if ($key === $depths[$depth]['i']) {
+                $depths[$depth]['i']++;
+            } else {
+                $depths[$depth]['i'] = max($key + 1, $depths[$depth]['i']);
+                $tokens[] = var_export($key, true);
+                $tokens[] = ' => ';
+            }
+
+            if (!\is_array($value)) {
+                $tokens[] = var_export($value, true);
+            } else if (empty($value)) {
+                $tokens[] = '[]';
+            }
+
+            $depths[$depth]['c']++;
+        }
+
+        $tokens[] = ']';
+        $tokens[] = \str_repeat(']', \count($depths) - 1);
+
+        return implode('', $tokens);
+    }
+}
---
+++ tests/fn/Composer/DI/Generator/ArrayExportTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * (c) php-fn
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fn\Composer\DI\Generator;
+
+use fn\test\assert;
+
+/**
+ * @covers ArrayExport
+ */
+class ArrayExportTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return array
+     */
+    public function providerToString(): array
+    {
+        return [
+            'empty' => ['[]', []],
+            'nested empty' => ['[[[]]]', [[[]]]],
+            'nested empty count > 1' => ['[[[], []], []]', [[[], []], []]],
+            'one element' => ["['a']", ['a']],
+            'mixed' => [
+                "[['a', 'b'], ['c', ['d', 'e' => 'e']], 'f']",
+                [['a', 'b'], ['c', ['d', 'e' => 'e']], 'f']
+            ],
+            'unordered numeric keys' => [
+                "[-5 => -5, 1 => 'a', 'b', 'k' => [2 => 'c'], 'd', 10 => 'e', 5 => 5, 6]",
+                [-5 => -5, 1 => 'a', 'b', 'k' => [2 => 'c'], 'd', 10 => 'e', 5 => 5, 6]
+            ],
+        ];
+    }
+
+    /**
+     * @covers       ArrayExport::__toString
+     *
+     * @dataProvider providerToString
+     *
+     * @param string $expected
+     * @param array $array
+     */
+    public function testToString(string $expected, array $array)
+    {
+        $export = new ArrayExport($array);
+        assert\same($expected, (string)$export);
+    }
+}
