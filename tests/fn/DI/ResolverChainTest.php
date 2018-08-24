7dc5fe81b36fe061b15e18942e7bb717ec539dba
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2018-08-24 11:20:46 +0200

    add test coverage for ResolverChain::call

--- tests/fn/DI/ResolverChainTest.php
+++ tests/fn/DI/ResolverChainTest.php
@@ -36,7 +36,7 @@ class ResolverChainTest extends \PHPUnit_Framework_TestCase
      */
     public function testReflect()
     {
-        $resolver = new ResolverChain(fn\di(['callback' => value(function(string $s1){})]));
+        $resolver = $this->resolver(['callback' => value(function(string $s1){})]);
         assert\type(\ReflectionFunction::class, $resolver->reflect('callback'));
         assert\type(\ReflectionMethod::class, $resolver->reflect([$this, __FUNCTION__]));
     }
@@ -46,18 +46,7 @@ class ResolverChainTest extends \PHPUnit_Framework_TestCase
      */
     public function testParameters()
     {
-        $resolver = new ResolverChain(
-            fn\di([\PHPUnit_Framework_TestCase::class => $this, static::class => $this]),
-            function(array $provided, \ReflectionParameter ...$parameters) {
-                $map = fn\map(array_change_key_case($provided));
-                return fn\traverse($parameters, function(\ReflectionParameter $parameter, &$key) use($map) {
-                    $key = $parameter->getPosition();
-                    return $map->get(strtolower($parameter->getName()), null);
-                });
-            },
-            new DefaultValueResolver
-        );
-
+        $resolver = $this->resolver([\PHPUnit_Framework_TestCase::class => $this, static::class => $this]);
         $callback = function(
             string $p1,
             self $p2,
@@ -69,6 +58,37 @@ class ResolverChainTest extends \PHPUnit_Framework_TestCase
         assert\equals(
             [1 => $this, 3 => $this, 2 => true, 0 => false],
             $resolver->parameters($callback, ['P3' => true, 'p1' => false])
+        );
+    }
+
+    /**
+     * @covers ResolverChain::call
+     */
+    public function testCall()
+    {
+        assert\same(
+            [$this, true, 'value'],
+            $this->resolver([static::class => $this])->call(
+                function(self $test, $default = true, $provided = null) {
+                    return [$test, $default, $provided];
+                },
+                ['provided' => 'value']
+            )
+        );
+    }
+
+    private function resolver(array $definition = []): ResolverChain
+    {
+        return new ResolverChain(
+            fn\di($definition),
+            function(array $provided, \ReflectionParameter ...$parameters) {
+                $map = fn\map(array_change_key_case($provided));
+                return fn\traverse($parameters, function(\ReflectionParameter $parameter, &$key) use($map) {
+                    $key = $parameter->getPosition();
+                    return $map->get(strtolower($parameter->getName()), null);
+                });
+            },
+            new DefaultValueResolver
         );
     }
 }
